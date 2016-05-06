02663a1a7503bc6279c89293c17e37eb1e6efbce
skelsec <info@skelsec.com>
2016-05-06 23:02:40 +0200

    making setup.py work

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+# THIS IS NOT A DEFAULT SETUP FILE!!!!
+#this is to be used when generating binary executable
+#note that you will need to copy the pyasn1 folder (from pyasn1 installation) to the site-packages folder!!!!
+
 from distutils.core import setup
 import py2exe
 
-setup(console=['kerberoastv2.py'])
+setup( options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+       console=['kerberoastv2.py'],
+	   zipfile = None, 
+	  )
